800c0a46fc47d846c8ab92cff8a0ab9911054fb2
tiagodeliberali <tiago.deliberali@gmail.com>
2016-05-23 14:47:52 -0300

    Overall refactoring

--- MusicSheetTrainer/MusicSheet.js
+++ MusicSheetTrainer/MusicSheet.js
@@ -1,48 +1,65 @@
 ﻿; (function (global) {
-    var size = 30;
-    var start = 200;
-    var notes = new Array();
     var noteElements = new Array();
-    var results = new Array();
-    var screenWidth = 0;
+    var testResults = new Array();
 
-    var event_OnFinish;
+    var options = {
+        noteSize: 30,
+        sheetStartPosition: 200, 
+        sheetWidth: 900,
+        canvasName: "#canvas",
+        onFinish: function (results) { },
+    };
+
+    var testInformation = {
+        notes: new Array(),
+        passRate: 100,
+        passTime: 0
+    };
+
+    var sheetStyle = {
+        noteStroke: "5px #555555",
+        noteStrokeCorrect: "5px #00b926",
+        noteStrokeWrong: "5px #ff0000",
+        lineStroke: "5px #0aa",
+        lineCap: "round"
+    }
 
     // 'new' an object
-    var MusicSheet = function (width) {
-        return new MusicSheet.init(width);
+    var MusicSheet = function (userOptions, userSheetStyle) {
+        return new MusicSheet.init(userOptions || {}, userSheetStyle || {});
     }
 
     MusicSheet.prototype = {
-        onFinish: function (toExecute) {
-            event_OnFinish = toExecute;
-        },
-
-        createTest: function (notes) {
+        createTest: function (userTestInformation) {
             this.clear();
 
-            for (var i in notes) {
-                this.createNote(notes[i]);
+            copyAttributes(testInformation, userTestInformation);
+
+            for (var i in testInformation.notes) {
+                this.createNote(testInformation.notes[i]);
             }
 
-            startTime = Date.now();
+            testInformation.startTime = Date.now();
         },
 
         createNote: function (note) {
-            var position = notes.length;
+            var position = noteElements.length;
 
             var element = canvas.display.ellipse({
                 x: this.getNodeCenterX(position),
                 y: this.getNodeCenterY(note),
-                radius_x: size / 2,
-                radius_y: size / 3,
-                stroke: "5px #555555"
+                radius_x: options.noteSize / 2,
+                radius_y: options.noteSize / 3,
+                stroke: sheetStyle.noteStroke
             });
 
             canvas.addChild(element);
-            notes[position] = note;
-            noteElements[position] = element;
+            noteElements[position] = {
+                note: note,
+                element: element
+            };
 
+            // draw lines above the sheet
             if (note > 11) {
                 for (var i = 12; i <= note; i++) {
                     if (i % 2 === 0) {
@@ -51,39 +68,37 @@
                 }
             }
 
+            // draw lines bellow the sheet
             if (note < 1) {
                 for (var i = 0; i >= note; i--) {
                     this.createSplitLine(i, position);
                 }
             }
-
-            return this;
         },
 
         clear: function () {
-            notes = new Array();
             noteElements = new Array();
-            results = new Array();
+            testResults = new Array();
 
             canvas.reset();
             this.drawSheetLines();
         },
 
         checkNote: function (note) {
-            var currentValidation = results.length;
+            var currentValidation = testResults.length;
 
-            if (notes[currentValidation] % 7 == note) {
-                results[currentValidation] = 1;
-                noteElements[currentValidation].stroke = "5px #00b926";
+            if (noteElements[currentValidation].note % 7 == note) {
+                testResults[currentValidation] = 1;
+                noteElements[currentValidation].element.stroke = sheetStyle.noteStrokeCorrect;
             }
             else {
-                results[currentValidation] = 0;
-                noteElements[currentValidation].stroke = "5px #ff0000";
+                testResults[currentValidation] = 0;
+                noteElements[currentValidation].element.stroke = sheetStyle.noteStrokeWrong;
             }
 
             canvas.redraw();
 
-            if (results.length == notes.length) {
+            if (testResults.length == noteElements.length) {
                 this.finishTest();
             }
         },
@@ -92,10 +107,10 @@
             var correct = 0;
             var error = 0;
             var total = 0;
-            var timeTaken = Date.now() - startTime;
+            var timeTaken = Date.now() - testInformation.startTime;
 
-            for (var i in results) {
-                if (results[i] === 1) {
+            for (var i in testResults) {
+                if (testResults[i] === 1) {
                     correct++;
                 }
                 else {
@@ -104,61 +119,75 @@
                 total++;
             }
 
-            var finalValues = {
+            var testResult = {
+                passed: (testInformation.passRate <= correct * 100 / total) && (testInformation.passTime === 0 || testInformation.passTime <= timeTaken),
                 correct: correct,
                 error: error,
                 total: total,
-                timeTaken: timeTaken
+                timeTaken: timeTaken,
+                testInformation: testInformation
             };
 
-            event_OnFinish(finalValues);
+            options.onFinish(testResult);
         },
 
         createSplitLine: function(note, position) {
             if (note % 2 === 0) {
                 canvas.addChild(canvas.display.line({
-                    start: { x: this.getNodeCenterX(position) - size, y: this.getNodeCenterY(note) },
-                    end: { x: this.getNodeCenterX(position) + size, y: this.getNodeCenterY(note) },
-                    stroke: "5px #0aa",
-                    cap: "round"
+                    start: { x: this.getNodeCenterX(position) - options.noteSize, y: this.getNodeCenterY(note) },
+                    end: { x: this.getNodeCenterX(position) + options.noteSize, y: this.getNodeCenterY(note) },
+                    stroke: sheetStyle.lineStroke,
+                    cap: sheetStyle.lineCap
                 }));
             }
         },
 
         getNodeCenterY: function(note) {
-            return start + size * 5 - note * size / 2;
+            return options.sheetStartPosition + options.noteSize * 5 - note * options.noteSize / 2;
         },
 
         getNodeCenterX: function(position) {
-            return size * 1.5 + position * size * 2.5;
+            return options.noteSize * 1.5 + position * options.noteSize * 2.5;
         },
 
         drawSheetLines: function () {
             for (var i = 0; i < 5; i++) {
                 canvas.addChild(canvas.display.line({
-                    start: { x: size / 2, y: size * i + start },
-                    end: { x: screenWidth, y: size * i + start },
-                    stroke: "5px #0aa",
-                    cap: "round"
+                    start: { x: options.noteSize / 2, y: options.noteSize * i + options.sheetStartPosition },
+                    end: { x: options.sheetWidth, y: options.noteSize * i + options.sheetStartPosition },
+                    stroke: sheetStyle.lineStroke,
+                    cap: sheetStyle.lineCap
                 }));
             }
-        }
+        },
+
+        copyAttributes: function (target, src) {
+            for (var key in src) {
+                if (src[key] !== undefined) {
+                    target[key] = src[key];
+                }
+            }
+
+            return target;
+        },
     };
 
     // the actual object is created here, allowing us to 'new' an object without calling 'new'
-    MusicSheet.init = function (width) {
+    MusicSheet.init = function (userOptions, userSheetStyle) {
         canvas = oCanvas.create({
-            canvas: "#canvas"
+            canvas: options.canvasName
         });
 
-        screenWidth = width;
+        copyAttributes(options, userOptions);
+        copyAttributes(sheetStyle, userSheetStyle);
+
         this.drawSheetLines();
     }
 
     // trick borrowed from jQuery so we don't have to use the 'new' keyword
     MusicSheet.init.prototype = MusicSheet.prototype;
 
-    // attach our Greetr to the global object, and provide a shorthand '$G' for ease our poor fingers
+    // attach our MusicSheet to the global object, and provide a shorthand 'MS$' for ease our poor fingers
     global.MusicSheet = global.MS$ = MusicSheet;
 
 }(window));
